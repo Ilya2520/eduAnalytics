3ab87b8ce5097d81372f34b5c489a85e4f9ebd14
Ilya2520 <91724425+Ilya2520@users.noreply.github.com>
2025-03-30 00:19:13 +0300

    Инициализация проекта

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class WelcomeController extends AbstractController
+{
+    #[Route('/welcome', name: 'app_welcome', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Добро пожаловать в Symfony приложение!',
+            'status' => 'success'
+        ]);
+    }
+}
